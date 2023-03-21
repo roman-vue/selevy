4c5e61cdc44c354c0c39b87f45813412d5545a94
roman-vue <roman.calderin.25@gmail.com>
2023-03-21 09:45:16 -0500

    :hammer:

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({envFilePath: '.env ', isGlobal:true}),DatabaseModule,UsersModule],
+  imports: [ConfigModule.forRoot({envFilePath: '.env ', isGlobal:true}),DatabaseModule,UsersModule, AuthModule],
 })
 export class AppModule {}
